13f0b7a22949c98607166cc7d4425fe885effd4f
Adam <ebokianknight@gmail.com>
2018-06-23 17:47:21 -0400

    Rename Contact Group method

--- app/models/contact_group.rb
+++ app/models/contact_group.rb
@@ -7,13 +7,13 @@ class ContactGroup < ApplicationRecord
   has_many :contacts
   has_one :location, as: :addressable
 
-  after_create :set_contact_name
+  after_create :set_contact_group_name
 
   accepts_nested_attributes_for :location, :venues, :contacts
 
   private
 
-  def set_contact_name
+  def set_contact_group_name
     return if name != "Anonymous"
     self.name = "Contact Group #{id&.to_s(16)}"
     save
