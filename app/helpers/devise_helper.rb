c4af5123cbc4e8637c483ed7622a34659614006e
Adam <ebokianknight@gmail.com>
2018-10-06 11:34:00 -0400

    Remove bindin.pry

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,13 +1,7 @@
 module DeviseHelper
   def devise_error_messages!
     return "" unless devise_error_messages?
-    binding.pry
-
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    # sentence = I18n.t("errors.messages.not_saved",
-    #                   :count => resource.errors.count,
-    #                   :resource => resource.class.model_name.human.downcase)
-    # <h2>#{sentence}</h2>
 
     html = <<-HTML
     <div id='error_explanation' class='has-errors'>#{messages}</div>
