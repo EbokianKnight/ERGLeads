812da3edcfbdbc912e3a5797fe31827a2ac59033
Adam <ebokianknight@gmail.com>
2019-01-23 03:41:41 -0500

    Update rails to 5.2.2, upgraded gemfile for security patches, replaced sass-rails.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if User.all.present?
+unless User.exists?
   User.create(
     email: Rails.application.credentials.initial_user[:email],
     password: Rails.application.credentials.initial_user[:password]
